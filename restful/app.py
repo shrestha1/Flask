9e551213b526a5a659bbef7d64e27c2af89778f3
Dipesh Shrestha <newardipesh@gmail.com>
2020-09-14 20:45:07 +0545

    adding bulb and getting by its id

--- restful/app.py
+++ restful/app.py
@@ -1,16 +1,47 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+parser = reqparse.RequestParser()
 
-class Product(Resource):
+bulbs = []
+
+bulb = {"bulb_id":1,
+        "of_room":"kitchen",
+        "status":"ON"
+        }
+
+class BulbItem(Resource):
+    '''
+    lets you get bulb by id
+    '''
+
+    def get(self, bulb_id):
+        return {"data":[bulb for bulb in bulbs if bulb["bulb_id"]==int(bulb_id)]}
+
+
+class BulbList(Resource):
+    '''
+    Lets you add bulb and get all bulbs 
+    '''
     def get(self):
-        return {"hello":"world"}
+            
+        return {"data":bulbs}
+
+    def post(self):
+        """
+         For adding the bulb
+        """
+        new_bulb = request.get_json(force=True)
+        bulbs.append(new_bulb)
+        return new_bulb
 
 
 
-api.add_resource(Product, '/')
+api.add_resource(BulbItem, '/api/bulb/<bulb_id>')
+api.add_resource(BulbList, '/api/bulb')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
